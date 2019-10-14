bd4c41d52308542c4715c8fc99c1191105a0b172
GabeIB <gabey300@gmail.com>
2019-10-14 10:34:50 -0900

    updated url - working minimum model

--- st_fakeiot.py
+++ st_fakeiot.py
@@ -10,6 +10,10 @@ from scapy.all import *
 import time
 
 conf.checkIPaddr = False
+
 fam,hw = get_if_raw_hwaddr(conf.iface)
-dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"),"end"])
-ans, unans = srp(dhcp_discover, multi=True)
+
+dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"), (161,"http://192.168.2.118/Manufacture/AmazonEcho.json"),"end"])
+#wireshark(dhcp_discover)
+#print(dhcp_discover.show())
+ans, unans = srp1(dhcp_discover, multi=True)
